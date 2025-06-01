ec18e9d6c81650b4556a6c8cf9f0420ec75eb548
gulsherkooner <gulsher.kooner@gmail.com>
2025-06-01 19:12:14 +0530

    .

--- my-app-dump.sql
+++ my-app-dump.sql
@@ -18,7 +18,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: enum_post_media_media_type; Type: TYPE; Schema: public; Owner: postgres
+-- Name: enum_post_media_media_type; Type: TYPE; Schema: public; Owner: postgres 
 --
 
 CREATE TYPE public.enum_post_media_media_type AS ENUM (
